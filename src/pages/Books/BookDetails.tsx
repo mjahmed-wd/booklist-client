f6e19b12aeb31f1900fc9fd4ea7a4a9f206dd258
Jubair <jubair@metadesignsolutions.co.uk>
2023-07-19 02:11:03 +0600

    book action buttons style added

--- src/pages/Books/BookDetails.tsx
+++ src/pages/Books/BookDetails.tsx
@@ -1,19 +1,13 @@
 import AddReview from '@/components/AddReview';
 import Review from '@/components/Review';
-import config from '@/config';
+import UserBookActions from '@/components/UserBookActions';
 import {
   useDeleteBookMutation,
   useGetSingleBookQuery,
 } from '@/redux/features/book/bookApi';
-import {
-  useAddToPlannedMutation,
-  useAddToWishlistMutation,
-  useGetUserInfoQuery,
-  useUpdatePlannedListMutation,
-} from '@/redux/features/user/userApi';
 import { useAppSelector } from '@/redux/hook';
 import { Container } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 type Props = {};
 
@@ -22,13 +16,6 @@ const BookDetails = (props: Props) => {
   const user = useAppSelector((state) => state.user);
   const { data } = useGetSingleBookQuery(bookId);
   const [deleteBook] = useDeleteBookMutation();
-  const [addToWishlist] = useAddToWishlistMutation();
-  const [addToPlannedList] = useAddToPlannedMutation();
-  const [updatePlannedList] = useUpdatePlannedListMutation();
-
-  const navigate = useNavigate();
-
-  const deleteHandler = () => deleteBook({ id: bookId! });
 
   return (
     <Container>
@@ -48,6 +35,9 @@ const BookDetails = (props: Props) => {
             </p>
             <br />
             <hr />
+            <UserBookActions/>
+            <br />
+            <hr />
             <h5 className="card-title">All Reviews</h5>
             <div className="card-group">
               {data?.reviews?.map((review) => (
@@ -60,49 +50,6 @@ const BookDetails = (props: Props) => {
             <AddReview />
           </div>
         </div>
-        {/* <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <button
-            type="button"
-            onClick={() =>
-              addToWishlist({
-                id: user.id,
-                data: {
-                  bookId: bookId!,
-                },
-              })
-            }
-          >
-            Add to Wishlist
-          </button>
-          <button
-            type="button"
-            onClick={() =>
-              addToPlannedList({
-                id: user.id,
-                data: {
-                  bookId: bookId!,
-                },
-              })
-            }
-          >
-            Add to reading list
-          </button>
-          <button
-            type="button"
-            onClick={() =>
-              navigate(`${config.routes.books.index}/${bookId}/edit`)
-            }
-          >
-            Edit
-          </button>
-          <button type="button" onClick={deleteHandler}>
-            Delete
-          </button>
-        </div> */}
-        {/* <h1>{data?.title}</h1>
-        <p>Author: {data?.author}</p>
-        <p>Genre: {data?.genre}</p>
-        <p>Publication: {data?.publicationDate}</p> */}
       </div>
     </Container>
   );
